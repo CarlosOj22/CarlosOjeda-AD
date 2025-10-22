8d5734c800d421781f7d0e40fb20e93419d3a7cf
CarlosOj22 <carlosojeda1336@gmail.com>
2025-10-22 08:49:38 +0200

    22-10-2025 Ejercicio Conectores Selects Y Delete

--- Tema 2-Conexiones/Ejercicio1-ConexionesJDBC/demo/src/main/java/com/example/ConectorGenerico.java
+++ Tema 2-Conexiones/Ejercicio1-ConexionesJDBC/demo/src/main/java/com/example/ConectorGenerico.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import java.sql.Statement;
+import java.util.Scanner;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConectorGenerico {
@@ -16,9 +20,32 @@ public class ConectorGenerico {
                     try {
                         String urljdbc = "jdbc:oracle:thin:carlos/1234@localhost:49161:XE";      
                         //la base de datos esta en local y el servicio Oracle es XE     
-                        //se utiliza thin driver              
+                        //se utiliza thin driver       
+                        //Conexion es = metodo estatico d drievrmanager getconection       
                         conOracle = DriverManager.getConnection(urljdbc, "carlos", "1234");
                         System.out.println("Conexion Realizada a Oracle correctamente.");
+                        //Creamos objeto sentencia que devuelve metodo create Statement de la conexion 
+                        Statement sentencia = conOracle.createStatement();
+                        //Consula string (FIJARTE BIEN EN NOMBRE COLUMNAS)
+                        String oracleConsulta = "SELECT * FROM Depart"; 
+                        //Objeto que leera los datos de las tablas,result set, con metodo ejecutar de la sentencia
+                        ResultSet resul = sentencia.executeQuery(oracleConsulta);
+                        //Recorremos objeto resultset
+                        //AMPLIACION SACAAR TODOS LOS EMPLEADOS DE ESE DEPARTAMENTO
+                        Statement sentenciaSacarEmpleados = conOracle.createStatement();
+                        System.out.println("NUM DEP----DEP NOMBRE----LOCALIDAD");
+                        while(resul.next()){
+                            System.out.println(resul.getInt(1) + "\t" + resul.getString(2) +"\t" +resul.getString(3));
+                            String sacarEmpleados= "SELECT APELLIDO FROM EMPLE WHERE DEPT_NO=" + resul.getInt(1);
+                            System.out.println("EMPLEADOS DE ESE DEPARTAMENTO: ");
+                            ResultSet empleados= sentenciaSacarEmpleados.executeQuery(sacarEmpleados);
+                            while(empleados.next()){
+                                System.out.println(empleados.getString(1));
+                            }
+                        }
+                        resul.close();
+                        sentencia.close();
+                        conOracle.close();
                     } catch (Exception e) {
                         System.out.println("No se ha podido realizar la conexion Oracle");
                     }
@@ -27,10 +54,23 @@ public class ConectorGenerico {
                 case "2":
 
                         Connection conMySql;       
+                        Scanner sc = new Scanner(System.in);
                         try {     
                             // Establecemos la conexion con la BD     
                             conMySql= DriverManager.getConnection("jdbc:mysql://localhost:33060/empresa", "root", "root");
                             System.out.println("Conexion Realizada a MySQL correctamente.");
+                            String consultaMySql = "DELETE FROM EMPLE WHERE DEPT_NO=?";
+                            PreparedStatement sentenciaMySql = conMySql.prepareStatement(consultaMySql);
+                            System.out.println("Introduce departamento para borrar sus empleados: ");
+                            int departamento=sc.nextInt();
+                            //AI QUE PASARLE PARAMETRO
+                            //VER LO DE MAS POSICIONES MAS LLAMADAS A SETINT PREGUNTAR
+                            //Y PREGUNTAR SI SE GUARDAN CAMBIOS EN LA BASE DED ATOS QUE AL SALIR ME DECIA DE GUARDAR O NO D¿EN DEVELOPER
+                            sentenciaMySql.setInt(1, departamento);
+                            //AHORA NO ES EXECUTE QUERY EN UPDATE
+                            sentenciaMySql.executeUpdate();
+                            System.out.println("Borrado con exito");
+
                     } catch (Exception e) {
                         System.out.println("No se ha podido realizar la conexion MySql");
                     }
